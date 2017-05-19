971ed6873aa6aba8a8ce6f81bb4aad82d6b1d99f
vishn412 <vishnarve@gmail.com>
2017-05-18 22:35:54 -0400

    [WIP]server socket bind

--- server.c
+++ server.c
@@ -3,5 +3,16 @@
 #include <uv.h>
 
 int main(char **argv, int argc) {
-	uv_tcp_t server;
+	/*Server Handler*/
+	uv_tcp_t *server = (uv_tcp_t *)malloc(sizeof(uv_tcp_t));
+	struct sockaddr_in *bind_addr;
+
+	/*Init*/
+	uv_tcp_init(uv_default_loop(), server);
+
+	/*address*/
+        int rc = uv_ip4_addr("127.0.0.1", 12345, bind_addr);
+
+	/* Socket Bind*/
+	uv_tcp_bind(server,(const struct sockaddr*)bind_addr, 0);
 }
