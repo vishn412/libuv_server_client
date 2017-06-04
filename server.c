53e9aabeec70788e8de69767efcb662f2db28f01
vishn412 <vishnarve@gmail.com>
2017-06-04 17:36:32 -0400

    [WIP]Server updates

--- server.c
+++ server.c
@@ -4,17 +4,22 @@
 
 uv_tcp_t *accept_client;
 
-void allocate_buffer(uv_handle_t* handle, size_t size, uv_buf_t* buf) {
+void allocate_buffer(uv_handle_t *handle, size_t size, uv_buf_t *buf) {
 	buf->base = malloc(size);
 	buf->len = size;
 }
 
-void read_cb(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf) {
+void read_cb(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
 }
 
-void accept_callback(uv_stream_t* server, int status) {
+void close_cb(uv_handle_t *handle) {
+	/*TODO check where to free mem of the handlers*/
+}
+
+void accept_callback(uv_stream_t *server, int status) {
 	 if (status < 0) {
 		 /*Error on the new connection*/
+		 uv_close((uv_handle_t *)server, close_cb);
 	 }
 	 else if (status == 0) {
 		 /*Success*/
@@ -26,7 +31,7 @@ void accept_callback(uv_stream_t* server, int status) {
 		 int rc = uv_accept(server, (uv_stream_t *)accept_client);
 		 if (rc < 0) {
 			 /*Error*/
-			 uv_close(accept_client, close_cb)
+			 uv_close((uv_handle_t *)accept_client, close_cb);
 		 }
 
 		 uv_read_start((uv_stream_t *)accept_client,
@@ -39,17 +44,17 @@ int main(char **argv, int argc) {
 	/*Server Handler*/
 	uv_tcp_t *server = (uv_tcp_t *)malloc(sizeof(uv_tcp_t));
 
-	struct sockaddr_in *bind_addr;
+	struct sockaddr_in bind_addr;
 	int rc;
 
 	/*Init*/
 	uv_tcp_init(uv_default_loop(), server);
 
 	/*address*/
-        rc = uv_ip4_addr("127.0.0.1", 12345, bind_addr);
+        rc = uv_ip4_addr("127.0.0.1", 12345, &bind_addr);
 
 	/* Socket Bind*/
-	uv_tcp_bind(server,(const struct sockaddr*)bind_addr, 0);
+	uv_tcp_bind(server,(const struct sockaddr*)&bind_addr, 0);
 
 	/*Socket Listen*/
 	uv_listen((uv_stream_t *)server, 10/*backlog*/, accept_callback);
@@ -57,4 +62,5 @@ int main(char **argv, int argc) {
 	/*Socket Accept*/
 
 	/*Read*/
+	uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 }
